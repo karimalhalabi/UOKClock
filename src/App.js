2191483c6e4de82670ead5bb291843c19608a5d7
karimalhalabi <karim@consultant.com>
2021-10-22 14:57:28 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ScriptTag from 'react-script-tag';
+
+function App() {
+  return (
+    <div className="App">
+      <ScriptTag type="text/javascript" src='sketches/clock/js/sketch.js' />
+    </div>
+  );
+}
+
+export default App;
